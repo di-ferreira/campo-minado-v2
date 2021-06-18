11abcd9205f0e81e936dd9797503ab88d43d1329
Diego Ferreira <diferreira.program@gmail.com>
2021-06-18 16:59:04 -0300

    Explosion event

--- src/io/github/diferreira/cm/visao/PainelTabuleiro.java
+++ src/io/github/diferreira/cm/visao/PainelTabuleiro.java
@@ -9,9 +9,18 @@ public class PainelTabuleiro extends JPanel {
     public PainelTabuleiro(Tabuleiro tabuleiro) {
         setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
 
-        tabuleiro.paraCadaCampo(c->add(new BotaoCampo(c)));
-        tabuleiro.registrarObservador(e->{
-            //TODO Mostrar Resultado para jogador
+        tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+        tabuleiro.registrarObservador(e -> {
+
+            SwingUtilities.invokeLater(() -> {
+                if (e.isGanhou()) {
+                    JOptionPane.showMessageDialog(this, "Parabéns você ganhou :)");
+                } else {
+                    JOptionPane.showMessageDialog(this, "Você perdeu :(");
+                }
+
+                tabuleiro.reiniciar();
+            });
         });
 
     }
